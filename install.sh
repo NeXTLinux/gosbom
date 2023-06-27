5b9f9048a12f86d1b03164867c3319dab4caeb0d
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-06-26 23:03:13 -0700

    up

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -u
 
 PROJECT_NAME="gosbom"
-OWNER=anchore
+OWNER=nextlinux
 REPO="${PROJECT_NAME}"
 GITHUB_DOWNLOAD_PREFIX=https://github.com/${OWNER}/${REPO}/releases/download
 INSTALL_SH_BASE_URL=https://raw.githubusercontent.com/${OWNER}/${PROJECT_NAME}
